85b173508b8e85fa7cd54ebc6da0bfa501f7abb8
Asier <blazgar@gmail.com>
2021-12-02 13:02:44 +0100

    started+

--- app/src/main/java/com/example/kafeosasungarria/Jolasa.java
+++ app/src/main/java/com/example/kafeosasungarria/Jolasa.java
@@ -2,7 +2,9 @@ package com.example.kafeosasungarria;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 
 public class Jolasa extends AppCompatActivity {
     static int pantallaAncho, pantallaAlto;
@@ -11,5 +13,9 @@ public class Jolasa extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jolasa);
+    }
+
+    public  void jolastu (View v){
+        
     }
 }
